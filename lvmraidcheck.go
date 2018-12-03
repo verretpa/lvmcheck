dbea49aafb8ee228bec3d3ce74db188a420903f3
Paul Verreth <paul@verreth.be>
2018-12-03 20:37:41 +0100

    ...

--- lvmraidcheck.go
+++ lvmraidcheck.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"reflect"
 )
 
 func main() {
-	cmd := exec.Command("ls", "-lah")
+	cmd := exec.Command("lvs")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
 	fmt.Printf("combined out:\n%s\n", string(out))
+
+	fmt.Println(reflect.TypeOf(out))
+
+	for ii, rr := range out {
+		fmt.Println(ii, rr)
+	}
 }
